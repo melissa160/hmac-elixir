ca1c72382804e4fbb892be66c14a506902fb63f9
melissa <melissa160@gmail.com>
2018-05-04 13:48:04 -0500

    final use

--- lib/hmac.ex
+++ lib/hmac.ex
@@ -1,35 +1,15 @@
 defmodule Hmac do
   def main do
-    message = ""
-    key = ""
-    message_hex = Base.encode16(message, case: :lower)
+    message = "{name: melissa}"
+    key = "135"
+    {sign, error_status} = System.cmd("helisaSign", [key, message])
 
-    IO.puts("hexadecimal message: ")
-    IO.puts(message)
-    # Base.encode16(key, case: :lower)
-    key_hex = :crypto.hash(:sha256, key)
-    IO.inspect(key_hex)
+    case error_status do
+      0 ->
+        {:ok, String.trim(sign)}
 
-    key_hmac =
-      :crypto.hmac_init(:sha256, key_hex)
-      |> :crypto.hmac_update(message_hex)
-      |> :crypto.hmac_final()
-      |> Base.encode16(case: :lower)
-
-    firma_key =
-      :crypto.hmac(:sha256, key, message)
-      |> Base.encode16(case: :lower)
-
-    IO.puts(firma_key)
-    IO.puts(key_hmac)
-    {key, error_status} = System.cmd("helisaSign", ["", ""])
-  end
-
-  def toBits(str) do
-    out = []
-    str = str |> String.replace(~r/\s|0x/, "")
-
-    len = String.length(str)
-    str = (str <> "00000000") |> String.slice(0, 8) |> String.to_integer(16)
+      _ ->
+        {:error, ""}
+    end
   end
 end
